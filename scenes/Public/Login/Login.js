701ff3e817a83dd0eeec6fda2d18c501ff48d685
Mateo Ceballos Bermudez <mateo.ceballos@inoutdelivery.com>
2021-06-08 07:52:46 -0500

    Change:  Modificación de estilos del login

--- scenes/Public/Login/Login.js
+++ scenes/Public/Login/Login.js
@@ -1,60 +1,94 @@
-import React, { useState, useRef, useLinking, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from "react-router-native";
-import { Container, Content, Item, Input, Button, Text, Spinner } from 'native-base';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from 'react-native';
 import { createForm } from 'rc-form';
 
-import i18n from '../../../i18n/i18n'
-import { auth as AuthActions } from '../../../services/Auth/AuthActions'
 
-const Login = ({ match, form }) => {
-  const dispatch = useDispatch()
-  const { getFieldDecorator, setFieldsValue } = form;
+const Login = () => {
 
-  const { loading, error } = useSelector(state => state.auth)
-  const [isFormError, setFormError] = useState()
-
-  const submit = (e) => {
-    if(!loading){
-      form.validateFields({ suppressWarning: true }, (error, value) => {
-        if (!error) {
-          setFormError(false)
-          dispatch(AuthActions.login(value.email, value.password))
-        } else
-          setFormError(true)
-      });
-    }
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed " + viewId);
   }
 
   return (
-    <Container>
-      <Content>
-        <Item regular>
-          {getFieldDecorator('email', { rules: [{ required: true }] })(
-            <Input type="email" placeholder={i18n.t('label.email')} onChangeText={email => setFieldsValue({ email })} />
-          )}
-        </Item>
-        <Item regular>
-          {getFieldDecorator('password', { rules: [{ required: true }] })(
-            <Input secureTextEntry placeholder={i18n.t('label.password')} onChangeText={password => setFieldsValue({ password })} />
-          )}
-        </Item>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Image style={styles.inputIcon} source={{ uri: 'https://png.icons8.com/message/ultraviolet/50/3498db' }} />
+        <TextInput style={styles.inputs}
+          placeholder="Email"
+          keyboardType="email-address"
+          underlineColorAndroid='transparent' />
+      </View>
 
-        {isFormError && <Text>{i18n.t('error.required')}</Text>}
-        {error.login && !isFormError && <Text>{i18n.t('error.login.' + error.login)}</Text>}
+      <View style={styles.inputContainer}>
+        <Image style={styles.inputIcon} source={{ uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db' }} />
+        <TextInput style={styles.inputs}
+          placeholder="Password"
+          secureTextEntry={true}
+          underlineColorAndroid='transparent' />
+      </View>
 
-        <Button success onPress={submit}>
-          { loading && <Spinner color='green'/> }
-          <Text>{i18n.t('button.login')}</Text>
-        </Button>
-
-        <Link to={`${match.url === '/' ? '' : match.url}/signup`} underlayColor="#f0f4f7">
-          <Text>¡{i18n.t('button.signup')}!</Text>
-        </Link>
-
-      </Content>
-    </Container>
+      <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} >
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableHighlight>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: 'white',
+  }
+});
+
 
 export default createForm()(Login)
